3074f05897c81f4145a7fb6f30aa395da6569996
thomasgoelles <thomas.goelles@gmail.com>
2022-01-30 14:47:55 +0000

    fixed import in snicar_driver

--- src/snicar_driver.py
+++ src/snicar_driver.py
@@ -30,7 +30,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-from .snicar_feeder import snicar_feeder
+from snicar_feeder import snicar_feeder
 
 # --------------------------------------------------------------------------------------
 # 1) Initialize Inputs of the model
