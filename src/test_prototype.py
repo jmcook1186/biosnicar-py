de02424782edb15e0a5f7e68f53ea4694e4ef38b
Joe <jmcook1186@gmail.com>
2022-02-18 11:05:02 +0000

    add some annotations to test prototype

--- src/test_prototype.py
+++ src/test_prototype.py
@@ -20,20 +20,23 @@ from plot import *
 ice, illumination, rt_config, model_config, plot_config, impurities = setup_snicar()
 status =validate_inputs(ice, rt_config, model_config, illumination, impurities)
 
-
+# array of vars to iterate over
 densList = [400, 500, 600, 700, 800]
 reffList = [200, 400, 600, 800, 1000]
 
-
+# set up loop
 for density in densList:
     for reff in reffList:
 
+        # update fields in existign class
         ice.rho = [density]*len(densList)
         ice.rds = [reff]*len(reffList)
         
+        # OP calcs
         ssa_snw, g_snw, mac_snw = get_layer_OPs(ice, impurities, model_config)
         tau, ssa, g, L_snw = mix_in_impurities(ssa_snw, g_snw, mac_snw, ice, impurities, model_config)
 
+        # get albedo
         outputs = toon_solver(tau, ssa, g, L_snw, ice, illumination, model_config, rt_config)
 
         print(outputs.BBA)
