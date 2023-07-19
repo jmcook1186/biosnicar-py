db2c79777c7a4eda56182d1b6d3206592b3c425d
Joe <jmcook1186@gmail.com>
2023-07-19 21:21:06 +0100

    rm redundant condition

--- src/biosnicar/snicar_driver.py
+++ src/biosnicar/snicar_driver.py
@@ -6,9 +6,7 @@ from pathlib import Path
 from get_albedo import get_albedo
 
 
-if __name__ == "__main__":
-    BIOSNICAR_SRC_PATH = Path(__file__).resolve().parent
-
-    # define input file
-    input_file = BIOSNICAR_SRC_PATH.joinpath("inputs.yaml").as_posix()
-    albedo = get_albedo(input_file, "adding-doubling", plot=True)
+BIOSNICAR_SRC_PATH = Path(__file__).resolve().parent
+# define input file
+input_file = BIOSNICAR_SRC_PATH.joinpath("inputs.yaml").as_posix()
+albedo = get_albedo(input_file, "adding-doubling", plot=True)
