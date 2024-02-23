8bf5321d6d8783b19898bb9d8fecf1cbb3b97460
Lou Chevrollier <77065045+lcvl41@users.noreply.github.com>
2024-02-23 18:47:01 +0100

    luts inv full spectra

--- experiments/LUTgenerator_ms1.py
+++ experiments/LUTgenerator_ms1.py
@@ -348,38 +348,53 @@ def call_SNICAR(z, density, grain_size, sza, lwc, alg, bc, dust):
 path_to_save_files = '/data/lou'
 
 ## PARAMS FOR CHLA ABS FEATURE 
-densities = [600] # maybe this can even be removed
-lwcs = [0.06] # maybe can be fixed or rm
-sza_list = ['diff'] # maybe can be removed, use diff instead
-algs = [0, 2.5e2, 5e2, 7.5e2,
-        1e3, 2e3, 3e3, 4e3, 5e3, 7.5e3,
-        1e4, 1.5e4, 2e4, 2.5e4, 3e4, 3.5e4, 4e4, 4.5e4, 5e4, 6e4, 7e4, 8e4, 9e4, 
-        1e5, 1.2e5,  1.4e5, 1.6e5, 1.8e5,  
-        2e5, 2.25e5, 2.5e5, 2.75e5, 
-        3e5, 3.25e5, 3.5e5, 3.75e5, 
-        4e5, 4.25e5, 4.5e5, 4.75e5, 
-        5e5, 6e5, 7e5, 8e5, 9e5,
-        1e6
-        #, 1.1e6, 1.2e6, 1.3e6, 1.4e6, 1.5e6,
-        # 1.6e6, 1.7e6, 1.8e6, 1.9e6, 2e6, 
-        # 2.2e6, 2.4e6, 2.6e6, 2.8e6, 3e6, 3.25e6, 3.5e6, 3.75e6, 4e6,
-        # 4.5e6, 5e6, 5.5e6, 6e6, 6.5e6, 7e6, 7.5e6, 8e6, 9e6, 1e7,
-        # 1.1e7, 1.2e7, 1.3e7, 1.4e7, 1.6e7, 1.8e7, 2.1e7, 2.5e7, 3.1e7, 4e7,
-        # 5e7, 6.5e7, 1e8, 2e8, 1e9
-        ] # 46
-algs = [int(a) for a in algs]
-grain_sizes = list(np.concatenate([np.arange(300, 500, 60),
-                                   np.arange(540, 900, 120),
-                              np.arange(900, 1500, 170), 
-                              np.arange(1580, 3500, 220)]))
-depths = [1]
-bcs = [0, 100, 200 , 300, 400,  
-      500, 600, 700,  800,  900, 
-      1000, 1100, 1200, 1300, 1400, 1500]
+# densities = [600] # maybe this can even be removed
+# lwcs = [0.06] # maybe can be fixed or rm
+# sza_list = ['diff'] # maybe can be removed, use diff instead
+# algs = [0, 2.5e2, 5e2, 7.5e2,
+#         1e3, 2e3, 3e3, 4e3, 5e3, 7.5e3,
+#         1e4, 1.5e4, 2e4, 2.5e4, 3e4, 3.5e4, 4e4, 4.5e4, 5e4, 6e4, 7e4, 8e4, 9e4, 
+#         1e5, 1.2e5,  1.4e5, 1.6e5, 1.8e5,  
+#         2e5, 2.25e5, 2.5e5, 2.75e5, 
+#         3e5, 3.25e5, 3.5e5, 3.75e5, 
+#         4e5, 4.25e5, 4.5e5, 4.75e5, 
+#         5e5, 6e5, 7e5, 8e5, 9e5,
+#         1e6
+#         #, 1.1e6, 1.2e6, 1.3e6, 1.4e6, 1.5e6,
+#         # 1.6e6, 1.7e6, 1.8e6, 1.9e6, 2e6, 
+#         # 2.2e6, 2.4e6, 2.6e6, 2.8e6, 3e6, 3.25e6, 3.5e6, 3.75e6, 4e6,
+#         # 4.5e6, 5e6, 5.5e6, 6e6, 6.5e6, 7e6, 7.5e6, 8e6, 9e6, 1e7,
+#         # 1.1e7, 1.2e7, 1.3e7, 1.4e7, 1.6e7, 1.8e7, 2.1e7, 2.5e7, 3.1e7, 4e7,
+#         # 5e7, 6.5e7, 1e8, 2e8, 1e9
+#         ] # 46
+# algs = [int(a) for a in algs]
+# grain_sizes = list(np.concatenate([np.arange(300, 500, 60),
+#                                    np.arange(540, 900, 120),
+#                               np.arange(900, 1500, 170), 
+#                               np.arange(1580, 3500, 220)]))
+# depths = [1]
+# bcs = [0, 100, 200 , 300, 400,  
+#       500, 600, 700,  800,  900, 
+#       1000, 1100, 1200, 1300, 1400, 1500]
 
-dusts = [0, 6e4, 1.25e5,  2e5,  3e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 1e6]
+# dusts = [0, 6e4, 1.25e5,  2e5,  3e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 1e6]
 
-# bc: below 1500 is OK. dust size 5: below 1e6 is ok. meshing to determine. 
+
+## PARAMS FOR RED SPECTRA ALL INVERSION
+densities = [600]
+sza_list = ['diff']
+depths = [1]  
+lwcs = [0.08]  # 4,  could be removed
+grain_sizes = list(np.concatenate([np.arange(560, 1000, 20), 
+                               np.arange(1000, 2000, 40), 
+                               np.arange(2000, 2260, 60)])) # 52
+algs = list(np.concatenate([np.arange(0, 1.95e4, 1.5e3), 
+                               np.arange(2e4, 5.75e4, 0.22e4), 
+                                np.arange(6e4, 9.7e4, 0.3e4),
+                               np.arange(1e5, 1.15e5, 0.5e4)])) # 47
+bcs = list(np.arange(0, 1575, 75)) # 21
+dusts = [[int(elem)] for elem in np.concatenate([np.arange(0, 4.7e5, 0.4e5), 
+                               np.arange(5e5, 1.25e6, 0.6e5)])] # 25
 
 ## PARAMS FOR RED SPECTRA NIR INVERSION
 # densities = [500+i*20 for i in range(0,11)]
@@ -523,22 +538,24 @@ dusts = [0, 6e4, 1.25e5,  2e5,  3e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 1e6]
 #############################################################################
 ################ CALL MODEL
 #############################################################################
-start = time.time()
-paramlist = list(set((itertools.product(depths, densities, grain_sizes, 
-                                          sza_list, lwcs, algs, bcs, dusts))))
-if __name__ == '__main__':
-    nb_cores = 100
-    pool = mp.Pool(nb_cores)
-    print(f'starting simulation on {nb_cores} cores')
-    data = pool.starmap(call_SNICAR, paramlist)
-    # pool.close()  
-    # pool.join() 
-    df = pd.DataFrame.from_records(data)
-    df = df.transpose()
-    df.columns = [str(i) for i in paramlist]
-    df.to_feather(f'{path_to_save_files}/022224_lut_snow_with_snw_alg.feather', 
-                  compression='zstd')
-    print('time for 1 lut: {}'.format(time.time() - start))
+
+for dust in dusts: 
+    if __name__ == '__main__':
+        start = time.time()
+        paramlist = list(set((itertools.product(depths, densities, grain_sizes, 
+                                                  sza_list, lwcs, algs, bcs, dust))))
+        nb_cores = 100
+        pool = mp.Pool(nb_cores)
+        print(f'starting simulation on {nb_cores} cores')
+        data = pool.starmap(call_SNICAR, paramlist)
+        # pool.close()  
+        # pool.join() 
+        df = pd.DataFrame.from_records(data)
+        df = df.transpose()
+        df.columns = [str(i) for i in paramlist]
+        df.to_feather(f'{path_to_save_files}/022324_lut_inversoin_snw_alg.feather', 
+                      compression='zstd')
+        print('time for 1 lut: {}'.format(time.time() - start))
 
 # for lwc in lwcs:
 #  	start = time.time()
